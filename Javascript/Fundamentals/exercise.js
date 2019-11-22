d01b8d2123a96663880760bcd1839832b1f0bf73
Tomi Sarjamo <tomi.sarjamo@tieto.com>
2019-11-22 15:54:01 +0200

    Moved file in different folder

---
+++ Javascript/Fundamentals/exercise.js
@@ -0,0 +1,45 @@
+console.log('\nJavaScript fundamentals')
+//JavaScript fundamentals Intermediate
+//Language features
+if (false) { //true=100 and false=undefined
+    var carId = 100;
+}
+console.log(carId)
+
+// const carId = 42;
+// console.log(carId);
+
+//Operators
+
+//Functions and scopes
+
+//Objects and Arrays
+
+//Classes and modules
+
+//Programming the BOM and DOM
+
+//Promises and error handling
+
+//Data access using HTTP
+
+//Forms
+
+//Security and Building production
+
+
+// let car = [
+//     {
+//         brand: "Ferrari",
+//         color: "Pink"
+//     },
+//     {
+//         brand: "Lada",
+//         color: "Red"
+//     }
+// ]
+// console.log('Hello from JS')
+// console.log('You have ' + car[0].color + ' ' + car[0].brand)
+// if (car[1].brand === "Lada") {
+//     console.log('You have ' + car[1].color + ' ' + car[1].brand)
+// }
