c6c826a1dcede026bbd902ab0d8bf62ccb170a76
Tomi Sarjamo <tomi.sarjamo@tieto.com>
2019-11-27 15:55:26 +0200

    Add files for new Objects and Prototypes course

---
+++ Javascript/ObjectsAndPrototypes/script.js
@@ -0,0 +1,7 @@
+/*JavaScript Objects and Prototypes : Jim Cooper Plunker: http://plnkr.co/edit/J81EK3TWFof0ekhEnyiJ?p=preview*/
+//1. Creating JavaScript Objects index.html display.js includes methods
+display('Hello');
+
+//2. JavaScript object properties
+
+//3. JavaScript prototypes and inheritance
