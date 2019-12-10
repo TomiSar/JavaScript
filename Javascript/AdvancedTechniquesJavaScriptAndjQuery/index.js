c05f1dcadfe7962684999e434fc411b655c57581
Tomi Sarjamo <tomi.sarjamo@tieto.com>
2019-12-10 15:40:43 +0200

    Advanced techniques and jQuery course started (initial commit).

---
+++ Javascript/AdvancedTechniquesJavaScriptAndjQuery/index.js
@@ -0,0 +1,16 @@
+//4. Using jQuery Deferred Objects
+//3. Advanced Event Handling
+//2. Event handling
+//1. Introduction
+
+//Default values in an object
+//Legacy support with object
+//Changing to an object parameter value
+//All default values
+//Default parameters
+//Stub functions
+//Draw: six parameters
+//Draw: four parameters
+//Extending draw : two parameters
+//Draw version 1.
+//Traditional JavaScript Functions
