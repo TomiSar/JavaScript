675f96c035976fd023190006f6db04d8684673ed
Tomi Sarjamo <tomi.sarjamo@tieto.com>
2019-12-11 15:06:07 +0200

    Started exercise in the first chapter

--- Javascript/AdvancedTechniquesJavaScriptAndjQuery/index.js
+++ Javascript/AdvancedTechniquesJavaScriptAndjQuery/index.js
@@ -1,16 +1,47 @@
-//4. Using jQuery Deferred Objects
-//3. Advanced Event Handling
-//2. Event handling
-//1. Introduction
+//Advanced techniques in JavaScript and jQuery -- Kevin Murray
 
-//Default values in an object
-//Legacy support with object
-//Changing to an object parameter value
-//All default values
-//Default parameters
-//Stub functions
-//Draw: six parameters
-//Draw: four parameters
-//Extending draw : two parameters
-//Draw version 1.
+//4. Using jQuery Deferred Objects
+//Using deferred Objects
+//Creating a deferred Object
+//Dynamic pages
+//Deferred mathods
+//Negative testing
+//LoadSection function
+//Deferred Object
+//Common pattern
+//Loading all content
+//Section 1 content
+//Layout
+//Traditional Ajax Processing
+//Promise
+//Outline
+
+//3. Advanced Event Handling
+//Event parameters
+//Custom events
+//Delegation
+//Namespace
+//Named functions
+//Event handler Methods
+//Outline
+
+//2. Event handling
+//No shorthand
+//Propagation
+//More elements
+//Message function
+//Shorthand methods
+//Outline
+
+//1. Introduction
+//Default values in an object -->
+//Legacy support with object -->
+//Changing to an object parameter value -->
+//All default values -->
+//Default parameters --> Draw-Version 6.html
+//Stub functions --> Draw-Version 5.html
+//Draw: six parameters --> Draw-Version 4.html
+//Draw: four parameters --> Draw-Version 3.html
+//Extending draw : two parameters --> Draw-Version 2.html
+//Draw version 1. --> Draw-Version 1.html
 //Traditional JavaScript Functions
