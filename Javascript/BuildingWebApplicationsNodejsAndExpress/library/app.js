0fee79ca7883b000aeada14115c67f648b3c1863
Tomi <tomi.sarjamo@tieto.com>
2020-03-26 17:06:31 +0200

    Books Rendering added in application

--- Javascript/BuildingWebApplicationsNodejsAndExpress/library/app.js
+++ Javascript/BuildingWebApplicationsNodejsAndExpress/library/app.js
@@ -8,19 +8,82 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
-// app.set('view engine', 'pug');
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // app.set('view engine', 'pug');
+
+const books = [
+  {
+    title: 'Sinuhe Egyptiläinen',
+    genre: 'Fiction',
+    author: 'Mika Waltari',
+    read: false
+  },
+  {
+    title: 'Tuulikaappimaa',
+    genre: 'Comedy Fiction',
+    author: 'Jari Tervo',
+    read: false
+  },
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'A prisoner of Azkaban',
+    genre: 'Fantasy',
+    author: 'JK Rowling',
+    read: false
+  },
+  {
+    title: 'The wind in the willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false
+  },
+  {
+    title: 'Life on the misses',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: false
+  },
+  {
+    title: 'Childhood',
+    genre: 'Biography',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  }];
+
+bookRouter.route('/').post()
+  .get((req, res) => {
+    res.render('books',
+      {
+        nav: [{ link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }],
+        title: 'Library',
+        books
+      });
+  });
+
+bookRouter.route('/single').post()
+  .get((req, res) => {
+    res.send('Hello single book');
+  });
+
+app.use('/books', bookRouter);
 
 // install eslint --> npm i -D eslint eslint-config-airbnb-base eslint-plugin-import
 // res.sendFile(path.join(`${__dirname}/views/index.html`));
+// res.render('index', { list: ['a', 'b'], title: 'Library' });
 app.get('/', (_req, res) => {
-  // res.render('index', { list: ['a', 'b'], title: 'Library' });
   res.render(
     'index',
     {
